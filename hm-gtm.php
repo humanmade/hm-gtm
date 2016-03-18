f21ad3d9f9c8f0ea757a6de57b71055423b3e92d
Andrey Filippov <andrey.filippov@sigma.software>
2016-03-18 12:11:45 +0200

    Code optimization.

--- hm-gtm.php
+++ hm-gtm.php
@@ -18,7 +18,6 @@ add_action( 'plugins_loaded', array( 'HM_GTM\Plugin', 'get_instance' ) );
  * Output the gtm tag, place this immediately after the opening <body> tag
  *
  * @param bool $echo
- * @param bool $network True to include network wide Google Tag Manager
  *
  * @return string
  */
@@ -26,13 +25,15 @@ function tag( $echo = true ) {
 
 	$id = get_option( 'hm_gtm_id', false );
 
-	$tag = '';
+	$network_id = get_site_option( 'hm_network_gtm_id' );
 
-	$data_layer = Plugin::data_layer();
+	if ( ! ( $id || $network_id ) ) {
+		return;
+	}
 
-	if ( $id ) {
-		$tag = sprintf( '
-			%2$s
+	$output = '';
+
+	$tag = '
 			<!-- Google Tag Manager -->
 			<noscript><iframe src="//www.googletagmanager.com/ns.html?id=%1$s" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 			<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\': new Date().getTime(),event:\'gtm.js\'});
@@ -40,37 +41,23 @@ function tag( $echo = true ) {
 				j.async=true;j.src=\'//www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);
 			})(window,document,\'script\',\'dataLayer\',\'%1$s\');</script>
 			<!-- End Google Tag Manager -->
-			',
-			esc_attr( $id ),
-			$data_layer
-		);
-		$data_layer = '';
+			';
+
+	echo Plugin::data_layer();
+
+	if ( $id ) {
+
+		$output .= sprintf( $tag, esc_attr( $id ) );
 	}
 
-	if ( is_multisite() ) {
+	if ( is_multisite() && $network_id ) {
 
-		$id = get_site_option( 'hm_network_gtm_id' );
-
-		if ( $id ) {
-			$tag .= sprintf( '
-				%2$s
-				<!-- Google Tag Manager - Network Wide -->
-				<noscript><iframe src="//www.googletagmanager.com/ns.html?id=%1$s" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-				<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\': new Date().getTime(),event:\'gtm.js\'});
-					var f=d.getElementsByTagName(s)[0], j=d.createElement(s), dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';
-					j.async=true;j.src=\'//www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);
-				})(window,document,\'script\',\'dataLayer\',\'%1$s\');</script>
-				<!-- End Google Tag Manager -->
-				',
-				esc_attr( $id ),
-				$data_layer
-			);
-		}
+		$output .= sprintf( $tag, esc_attr( $network_id ) );
 	}
 
 	if ( $echo ) {
-		echo $tag;
+		echo $output;
 	}
 
-	return $tag;
+	return $output;
 }
